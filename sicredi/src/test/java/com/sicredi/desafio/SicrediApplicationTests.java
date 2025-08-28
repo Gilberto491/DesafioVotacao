dc597c98b0b9921f5c0bd0d5f3b3727a0650928f
g.junior <gilberto.fredes@totvs.com.br>
2025-08-28 19:51:18 -0300

    chore: projeto inicial com Spring Boot startando

---
+++ sicredi/src/test/java/com/sicredi/desafio/SicrediApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sicredi.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SicrediApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
