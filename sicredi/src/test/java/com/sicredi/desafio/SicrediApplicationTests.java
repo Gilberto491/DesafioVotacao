5860fa1e54e35c2c15d13b74f5288e2f55313102
g.junior <gilberto.fredes@totvs.com.br>
2025-08-30 21:38:15 -0300

    fix: ajuste pipeline

--- sicredi/src/test/java/com/sicredi/desafio/SicrediApplicationTests.java
+++ sicredi/src/test/java/com/sicredi/desafio/SicrediApplicationTests.java
@@ -2,8 +2,10 @@ package com.sicredi.desafio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SicrediApplicationTests {
 
     @Test
